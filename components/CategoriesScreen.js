51303587be232a15c105933c790aea81c3c638e8
brennolvs <brenno.niit@gmail.com>
2023-10-18 03:07:12 -0300

    Subindoo código

---
+++ components/CategoriesScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+
+import data from '../db.json';
+
+const CategoriasScreen = ({ navigation }) => {
+  const categorias = data.categorias;
+
+  const handleCategoriaPress = (categoria) => {
+    navigation.navigate('Produtos', { categoria });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>O Que Deseja Para Hoje?</Text>
+      <FlatList
+        data={categorias}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.categoriaItem}
+            onPress={() => handleCategoriaPress(item)}
+          >
+            <Text style={styles.categoriaText}>{item.nome}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  categoriaItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 5,
+  },
+  categoriaText: {
+    fontSize: 16,
+  },
+});
+
+export default CategoriasScreen;
---
+++ components/ProductsList.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, ImageBackground } from 'react-native';
+
+import data from '../db.json';
+
+const ProdutosScreen = ({ route }) => {
+  const { categoria } = route.params;
+  const produtos = data.produtos.filter((produto) => produto.categoria_id === categoria.id);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Produtos da categoria: {categoria.nome}</Text>
+      <FlatList
+        data={produtos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+
+            <View style={styles.produtoItem}>
+              <ImageBackground source={{ uri: item.image }} style={styles.image}/>
+              <Text style={styles.produtoNome}>{item.nome}</Text>
+              <Text style={styles.produtoDescricao}>Descrição: {item.descricao}</Text>
+              <Text style={styles.produtoPreco}>Preço: R$ {item.preco.toFixed(2)}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  produtoItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 5,
+  },
+  produtoNome: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  produtoDescricao: {
+    fontSize: 14,
+  },
+  produtoPreco: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  image: {
+    width: '100%',
+    height: 200
+  }
+});
+
+export default ProdutosScreen;
